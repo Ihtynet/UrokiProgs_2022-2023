91c8a7d45ca63b9a462841112cadb228095a0cd8
Владимир <50101573+Ihtynet@users.noreply.github.com>
2022-10-04 08:00:16 +0300

    123

--- 01_10_2022/paint_krug.py
+++ 01_10_2022/paint_krug.py
@@ -4,9 +4,11 @@ root = Tk()
 root.title("Урок 10-10-2022")
 root.geometry("300x300")
 
-canvas = Canvas(bg="white", width=250, height=250)
+canvas = Canvas(bg="white", width=260, height=260)
 canvas.pack(anchor=CENTER, expand=1)
 
-canvas.create_oval(60,60,110,210)
-
+canvas.create_oval(10,10,250,250)
+canvas.create_oval(60,60,100,100)
+canvas.create_oval(160,60,200,100)
+canvas.create_oval(60,160,200,200)
 root.mainloop()
