86d627433dc6cbb689e81ffac893622e0cac197c
Владимир <50101573+Ihtynet@users.noreply.github.com>
2022-10-09 12:35:12 +0300

    123

--- 08_10_2022/paint_starrysky.py
+++ 08_10_2022/paint_starrysky.py
@@ -3,25 +3,30 @@ from tkinter import *
 
 root = Tk()
 root.title("Урок 10-10-2022")
-root.geometry("700x700")
+root.geometry("610x610")
 
 canvas = Canvas(bg="black", width=600, height=600)
 canvas.pack(anchor=CENTER, expand=1)
 
+###################################
+# Эот код рисует 700 мелких звезд
 for i in range(700):
     x1 = random.randint(1,600)
     y1 = random.randint(1,600)
     r = random.randint(1,5)
     canvas.create_oval(x1, y1, x1+r, y1+r, fill="white")
 
+###################################
+# Эот код рисует 20 крупных звезд
 for i in range(20):
     x1 = random.randint(1,600)
     y1 = random.randint(1,600)
     r = random.randint(5,10)
     canvas.create_oval(x1, y1, x1+r, y1+r, fill="white")
 
+###################################
+# Эот код рисует сатурн
 canvas.create_oval(200, 200, 250, 250, fill="white")
-
 dx=0
 dy=0
 for k in range(10):
@@ -29,5 +34,4 @@ for k in range(10):
     dx=dx+3
     dy=dy+2
 
-canvas.create_oval(200, 200, 250, 250, fill="white", )
 root.mainloop()
