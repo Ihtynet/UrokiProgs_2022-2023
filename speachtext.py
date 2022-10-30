49f6da9de4190202467664fae83fdc99c0e9522f
Владимир <50101573+Ihtynet@users.noreply.github.com>
2022-10-30 23:19:25 +0300

    123

---
+++ speachtext.py
@@ -0,0 +1,11 @@
+import pyttsx3
+engine = pyttsx3.init()
+#engine.say('Ваня! Учи математику. ТЫ давно не смотрел ролики с ютюба!')
+#engine.say('Какой ролик смотрел?')
+#engine.say('Что ты понял? Что такое круги эйллера? ')
+engine.say("Спят усталые игрушки!  " )
+engine.say("Книжки спят! " )
+engine.say("Одеяла и падушки! " )
+engine.say("О-о-о-чень грязные и нужно постирать")
+engine.runAndWait()
+
